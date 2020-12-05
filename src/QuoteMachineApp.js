feb87213b334c7a0f7288a8924aee5d76c3ac0d4
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-12-05 11:35:06 -0600

    feat: add react quote machine app
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ src/QuoteMachineApp.js
@@ -0,0 +1,56 @@
+const quotes = [
+  {text: "Oops.", author: "John Dow"},
+  {text: "My bad, yo.", author: "Rory Smith"},
+  {text: "I did that.", author: "Steve Urkel"},
+  {text: "I can smell what the Rock is cooking.", author: "Steve Austin"},
+  {text: "The nut doesn't fall far from the nut.", author: "Jeremy Gray"},
+  {text: "Say again?", author: "Dr Who"},
+];
+
+const defaultState = {index: Math.floor(Math.random() * quotes.length)};
+
+class QuoteMachine extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      index: Math.floor(Math.random() * quotes.length)
+    };
+    
+    this.newQuote = this.newQuote.bind(this);
+  }
+
+  newQuote()
+  {
+    this.setState({index: Math.floor(Math.random() * quotes.length)});
+  }
+
+  render()
+  {
+    return (<div id="quote-box">
+            <div id="text">
+            <p>
+            {quotes[this.state.index].text}
+            </p>
+            </div>
+            <div id="author">
+            <p>
+            &mdash;{quotes[this.state.index].author}
+            </p>
+            </div>
+            <div id="controls">
+            <div id="buttons">
+            <button type="button" onClick={this.newQuote} id="new-quote">
+            New Quote
+            </button>
+            <a href="twitter.com/intent/tweet" id="tweet-quote">
+            Tweet Quote
+            </a>
+            </div>
+            </div>
+            </div>);
+  }
+}
+
+ReactDOM.render(<QuoteMachine/>, document.getElementById("quoteMachine"));
